d4251d789ada3787ba61d64a6e75f92c173f19e2
luiscrespoo <80181219+luiscrespoo@users.noreply.github.com>
2023-12-04 19:01:04 +0100

    pago de menu

--- public/js/pago.js
+++ public/js/pago.js
@@ -5,30 +5,60 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 });
 
-
 function checkAndPay() {
     fetch('/api/checkSaldo')
         .then(response => response.json())
         .then(data => {
+            const messageContainer = document.getElementById('message-container');
+            const payButton = document.getElementById('pay-button');
             if (data.saldo >= 3.5) {
-                if (confirm('¿Confirmas el pago del menú?')) {
-                    realizarPago();
-                }
+                payButton.style.display='none';
+                messageContainer.innerHTML = `
+                    <p>¿Confirmas el pago del menú?</p>
+                    <button onclick="realizarPago()">✅</button>
+                    <button onclick="cancelarPago()">❌</button>
+                `;
             } else {
-                alert('Saldo insuficiente');
+                showMessage('Saldo insuficiente');
             }
         })
-        .catch(error => console.error('Error:', error));
+        .catch(error => {
+            console.error('Error:', error);
+            showMessage('Error al verificar el saldo');
+        });
 }
+
 
 function realizarPago() {
     fetch('/api/realizarPago', { method: 'POST' })
         .then(response => {
+            const messageArea = document.getElementById('message-area');
             if (response.ok) {
-                alert('Pago realizado con éxito');
+                showMessage('Pago realizado con éxito');
             } else {
-                alert('Error al realizar el pago');
+                showMessage('Error al realizar el pago');
             }
         })
-        .catch(error => console.error('Error:', error));
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById('message-area').innerHTML = `<p>Error al realizar el pago</p>`;
+        });
+}
+
+function cancelarPago() {
+    showMessage('Pago cancelado');
+}
+
+function showMessage(message) {
+    const messageContainer = document.getElementById('message-container');
+    const payButton = document.getElementById('pay-button');
+
+    messageContainer.innerHTML = `<p>${message}</p>`;
+    payButton.style.display = 'none'; // Ocultar botón de pago
+
+    // Restablecer después de 5 segundos
+    setTimeout(() => {
+        messageContainer.innerHTML = ''; // Limpiar mensaje
+        payButton.style.display = 'block'; // Mostrar botón de pago
+    }, 5000);
 }
