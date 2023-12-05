b0be3dffc6a79afb7a3208d5eb75e5d9de8bff6b
alvarocq1 <98234781+alvarocq1@users.noreply.github.com>
2023-12-05 18:22:24 +0100

    Update agendarCitas.js

--- public/js/agendarCitas.js
+++ public/js/agendarCitas.js
@@ -1,35 +1,49 @@
-// Obtener referencias a los elementos del DOM
-const btnBuscarCitas = document.getElementById('buscar-citas');
-const citasDisponiblesContenedor = document.getElementById('citas-disponibles');
-const inputFechaCita = document.getElementById('fecha-cita');
+function formatearFecha(fecha) {
+    const opciones = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let fechaFormateada = new Date(fecha).toLocaleDateString('es-ES', opciones);
 
-// Evento para buscar citas disponibles
-btnBuscarCitas.addEventListener('click', function() {
-    let fechaSeleccionada = inputFechaCita.value; // Define la variable aquí para que esté disponible en este contexto
-    if (!fechaSeleccionada) {
-        alert('Por favor, selecciona una fecha.');
-        return;
-    }
+    // Dividir la fecha en partes y convertir el día de la semana a mayúsculas
+    const partes = fechaFormateada.split(' ');
+    partes[0] = partes[0].toUpperCase(); // Convierte el día de la semana a mayúsculas
 
-    // Hacer la solicitud al endpoint para obtener las citas disponibles
-    fetch(`/citas/disponibles/${fechaSeleccionada}`)
+    // Volver a juntar las partes y devolver la fecha formateada
+    return partes.join(' ');
+}
+
+// Función para cargar las citas disponibles de una fecha seleccionada
+function cargarCitasDisponibles(fecha) {
+    fetch(`/citas/disponibles/${fecha}`)
         .then(response => response.json())
         .then(citas => {
-            citasDisponiblesContenedor.innerHTML = ''; // Limpiar contenedor
+            // Convertir la fecha a formato legible
+            const fechaLegible = formatearFecha(fecha);
+    
+           // Selecciona el nuevo contenedor para el título
+           const tituloCitasDisponiblesContenedor = document.getElementById('titulo-citas-disponibles');
+           tituloCitasDisponiblesContenedor.classList.remove('oculto');
+           tituloCitasDisponiblesContenedor.classList.add('visible1');
+           tituloCitasDisponiblesContenedor.innerHTML = `<h2>Citas disponibles para el ${fechaLegible}</h2>`;
+
+           // Selecciona el contenedor para las citas disponibles
+           const citasDisponiblesContenedor = document.getElementById('citas-disponibles');
+           citasDisponiblesContenedor.classList.remove('oculto');
+           citasDisponiblesContenedor.classList.add('visible2');
+           citasDisponiblesContenedor.innerHTML = ''; // Limpia el contenedor para nuevas citas
             citas.forEach(cita => {
                 const citaDiv = document.createElement('div');
                 citaDiv.className = cita.ocupado ? 'cita-disponible reservado' : 'cita-disponible';
-                citaDiv.textContent = `Hora: ${cita.hora_inicio} - ${cita.hora_fin}`;
+                citaDiv.textContent = `${cita.hora_inicio} - ${cita.hora_fin}`;
                 if (!cita.ocupado) {
                     citaDiv.dataset.id = cita.id;
-                    citaDiv.addEventListener('click', () => reservarCita(cita.id, fechaSeleccionada, cita.hora_inicio)); // Usa fechaSeleccionada aquí
+                    citaDiv.addEventListener('click', () => reservarCita(cita.id, fecha, cita.hora_inicio));
                 }
                 citasDisponiblesContenedor.appendChild(citaDiv);
             });
+        })
+        .catch(error => {
+            console.error('Error al obtener citas disponibles:', error);
         });
-});
-
-
+}
 
 // Función para reservar una cita
 // Esta función se llamará cuando un usuario haga clic en una cita disponible
@@ -67,17 +81,29 @@ function reservarCita(citaId, fecha, horaInicio) {
     });
 }
 
-// Suponiendo que ya tienes una función que carga las citas disponibles y les asigna eventos de clic
-function cargarCitasDisponibles(fecha) {
-    // ...código para cargar citas disponibles...
+// Función para cargar las fechas disponibles al cargar la página
+function cargarFechasDisponibles() {
+    fetch('/api/fechas-citas-disponibles')
+        .then(response => response.json())
+        .then(data => {
+            const contenedorFechas = document.getElementById('fechas-container');
+            contenedorFechas.innerHTML = ''; // Limpiar el contenedor antes de agregar nuevas fechas
 
-    // Suponiendo que citas es un arreglo de citas disponibles que recibiste del servidor
-    citas.forEach(cita => {
-        const citaDiv = document.createElement('div');
-        citaDiv.className = 'cita-disponible';
-        citaDiv.textContent = `Hora: ${cita.hora_inicio} - ${cita.hora_fin}`;
-        citaDiv.dataset.id = cita.id; // Suponiendo que el servidor envía el ID de la cita
-        citaDiv.addEventListener('click', () => reservarCita(cita.id, fecha, cita.hora_inicio));
-        // Agregar el div de cita al contenedor de citas disponibles
-    });
+            data.fechas.forEach(fecha => {
+                const fechaDiv = document.createElement('div');
+                fechaDiv.className = 'fecha';
+                fechaDiv.setAttribute('data-fecha', fecha);
+                fechaDiv.textContent = formatearFecha(fecha);
+                fechaDiv.addEventListener('click', function() {
+                    cargarCitasDisponibles(fecha);
+                });
+                contenedorFechas.appendChild(fechaDiv);
+            });
+        })
+        .catch(error => {
+            console.error('Error al obtener fechas de citas:', error);
+        });
 }
+
+// Llamamos a cargarFechasDisponibles al cargar la página y eliminamos el botón buscar citas ya que no es necesario
+document.addEventListener('DOMContentLoaded', cargarFechasDisponibles);
