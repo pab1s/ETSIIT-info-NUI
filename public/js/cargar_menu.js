b69de5cc19abc28f0d0953721f791c752ce27b89
luiscrespoo <80181219+luiscrespoo@users.noreply.github.com>
2023-12-05 16:00:59 +0100

    cambios en la navegabilidad de comedores (se añaden flechas)

--- public/js/cargar_menu.js
+++ public/js/cargar_menu.js
@@ -3,13 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.json())
         .then(data => {
             crearBotonesComedores(data);
-            // Después de crear botones, selecciona el primero
-            const primerBoton = document.querySelector('.comedor-button');
-            if (primerBoton) {
-                seleccionarBoton(primerBoton, true);
+            const primerBotonComedor = document.querySelector('.comedor-button');
+            if (primerBotonComedor) {
+                seleccionarBoton(primerBotonComedor, true); // Selecciona el primer comedor
             }
         });
 });
+
 
 let estadoSeleccion = {
     comedor: null,
@@ -58,7 +58,6 @@ function mostrarDias(dates, comedorButton) {
         }
 
         diaButton.onclick = () => {
-            menuContainer.style.display = 'none';
             seleccionarBoton(diaButton, false);
         }
 
@@ -107,10 +106,7 @@ function mostrarMenus(menus, esPasado) {
 
         // Agregar el párrafo al contenedor
         menusDetailContainer.appendChild(mensaje);
-    }else {
-        payButton.style.display = 'block';
     }
-
 
 }
 
@@ -151,39 +147,29 @@ function obtenerMesEnNumero(mesTexto) {
 }
 
 function seleccionarBoton(boton, esComedor) {
-    // Eliminar la clase 'boton-seleccionado' de todos los botones relevantes
     if (esComedor) {
         document.querySelectorAll('.comedor-button').forEach(b => b.classList.remove('boton-seleccionado'));
-        estadoSeleccion.comedor = boton.innerText; // Actualizar el comedor seleccionado
-        const infoComedor = JSON.parse(boton.dataset.info); // Obtener información del comedor
-        mostrarDias(infoComedor.dates, boton); // Mostrar los días para el comedor seleccionado
-        // Seleccionar el primer día no pasado
+        estadoSeleccion.comedor = boton.innerText;
+        const infoComedor = JSON.parse(boton.dataset.info);
+        mostrarDias(infoComedor.dates, boton);
         const primerDiaNoPasado = encontrarPrimerDiaNoPasado(infoComedor.dates);
         setTimeout(() => {
             const botonDia = document.querySelector(`.dia-button[data-dia="${primerDiaNoPasado}"]`);
             if (botonDia) {
-                seleccionarBoton(botonDia, false); // Falso porque es un botón de día
+                seleccionarBoton(botonDia, false);
             }
         }, 0);
-
     } else {
         document.querySelectorAll('.dia-button').forEach(b => b.classList.remove('boton-seleccionado'));
-        estadoSeleccion.fecha = boton.innerText; // Actualizar la fecha seleccionada
+        estadoSeleccion.fecha = boton.dataset.dia;
+        console.log(estadoSeleccion.fecha);
         const infoDia = JSON.parse(boton.dataset.info);
-        mostrarMenus(infoDia, boton);
+        mostrarMenus(infoDia, verificarDiaPasado(estadoSeleccion.fecha));
     }
+    boton.classList.add('boton-seleccionado');
+    boton.focus();
+}
 
-    boton.classList.add('boton-seleccionado'); // Resaltar el botón seleccionado
-    boton.focus(); // Enfocar el botón seleccionado
-}
-function mostrarDiasComedorSeleccionado() {
-    const comedorSeleccionado = document.querySelector('.comedor-button.boton-seleccionado');
-    console.log(comedorSeleccionado.dataset.info);
-    if (comedorSeleccionado) {
-        const infoComedor = JSON.parse(comedorSeleccionado.dataset.info);
-        mostrarDias(infoComedor.dates, comedorSeleccionado); // Mostrar los días para el comedor seleccionado
-    }
-}
 
 function encontrarPrimerDiaNoPasado(dates) {
     for (const dia of Object.keys(dates)) {
@@ -195,55 +181,28 @@ function encontrarPrimerDiaNoPasado(dates) {
 }
 
 
-let enfoqueEnComedores = true; // Para saber si el enfoque está en los comedores o en los días
-
 document.addEventListener('keydown', function(event) {
     const botonesComedores = document.querySelectorAll('.comedor-button');
     const botonesDias = document.querySelectorAll('.dia-button');
-    let indiceActual, botones;
-
-    if (enfoqueEnComedores) {
-        botones = botonesComedores;
-    } else {
-        botones = botonesDias;
-    }
-
-    indiceActual = Array.from(botones).findIndex(boton => boton.classList.contains('boton-seleccionado'));
+    let indiceActualDia = Array.from(botonesDias).findIndex(boton => boton.classList.contains('boton-seleccionado'));
+    let indiceActualComedor = Array.from(botonesComedores).findIndex(boton => boton.classList.contains('boton-seleccionado'));
 
     event.preventDefault();
 
     switch (event.key) {
         case 'ArrowRight':
-            indiceActual = (indiceActual + 1) % botones.length;
+            indiceActualDia = (indiceActualDia + 1) % botonesDias.length;
+            seleccionarBoton(botonesDias[indiceActualDia], false);
             break;
         case 'ArrowLeft':
-            indiceActual = (indiceActual - 1 + botones.length) % botones.length;
-            break;
-        case 'ArrowDown':
-            if (enfoqueEnComedores) {
-                mostrarDiasComedorSeleccionado(); // Implementar esta función
-                enfoqueEnComedores = false;
-                const primerBoton = document.querySelector('.dia-button');
-                if (primerBoton) {
-                    seleccionarBoton(primerBoton);
-                }
-                return;
-            }
+            indiceActualDia = (indiceActualDia - 1 + botonesDias.length) % botonesDias.length;
+            seleccionarBoton(botonesDias[indiceActualDia], false);
             break;
         case 'ArrowUp':
-            if (!enfoqueEnComedores) {
-                const menuContainer = document.getElementById('menu-container');
-                if (menuContainer) {
-                    menuContainer.style.display = 'none'; // Ocultar el menú de días
-                }
-                enfoqueEnComedores = true;
-                return;
-            }
+            indiceActualComedor = (indiceActualComedor + 1) % botonesComedores.length;
+            seleccionarBoton(botonesComedores[indiceActualComedor], true);
             break;
-        default:
-            return;
     }
-
-    seleccionarBoton(botones[indiceActual], enfoqueEnComedores);
 });
+
 
